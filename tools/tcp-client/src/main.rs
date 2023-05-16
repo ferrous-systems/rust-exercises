0d1ad784349da2ac478ebd3c2d2c708a25f419b1
Mirabellensaft <tanks@ferrous-systems.com>
2023-05-16 16:01:01 +0200

    add the tcp client

---
+++ tools/tcp-client/src/main.rs
@@ -0,0 +1,27 @@
+use std::io::prelude::*;
+use std::net::{Shutdown, TcpStream};
+
+fn main() -> std::io::Result<()> {
+    let arg = std::env::args().nth(1);
+
+    let message = match arg {
+        Some(msg) => msg,
+        None => String::from("Hello!"),
+    };
+    // or:
+    // arg.unwrap_or_default(String::from("Hello!"));
+
+    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
+
+    writeln!(stream, "{}", message)?;
+
+    stream.shutdown(Shutdown::Write)?;
+
+    let mut buffer = String::new();
+
+    stream.read_to_string(&mut buffer)?;
+
+    println!("{}", buffer);
+
+    Ok(())
+}
