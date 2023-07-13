6b67650abe64f909e28379fd8556618c8c264256
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2023-07-13 11:56:03 +0100

    Use a slug as the output name.
    Borrows from rust-training #69, but our build script is a little more complicated.

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+OUTPUT_NAME=${1:-./output}
+
 # Build and test the solutions
 pushd exercise-solutions
 cargo test
@@ -24,9 +26,9 @@ mdbook test
 mdbook build
 popd
 
-rm -rf ./output
-mkdir -p ./output
-mkdir -p ./output/exercise-book
+rm -rf "${OUTPUT_NAME}"
+mkdir -p "${OUTPUT_NAME}"
+mkdir -p "${OUTPUT_NAME}/exercise-book"
 # Note: the use of the html subdirectory here is deliberate.
 # a) it allows the book to be provided as PDF in the future
 # b) it ensures the `../../exercise-solutions` links in the markdown also work
@@ -34,10 +36,10 @@ mkdir -p ./output/exercise-book
 #    because the Markdown book source lives in the `src` subfolder and so you
 #    have to go up one extra level. Adding an extra level in the output
 #    is easier than re-writing all the links at build time.
-mv ./exercise-book/book ./output/exercise-book/html
-cp -r ./exercise-templates ./output/
-rm -rf ./output/exercise-templates/target
-cp -r ./exercise-solutions ./output/
-rm -rf ./output/exercise-solutions/target
-rm -rf ./output/exercise-solutions/connected-mailbox/target
-zip -r ./output.zip ./output
+mv ./exercise-book/book "${OUTPUT_NAME}/exercise-book/html"
+cp -r ./exercise-templates "${OUTPUT_NAME}/"
+rm -rf "${OUTPUT_NAME}/exercise-templates/target"
+cp -r ./exercise-solutions "${OUTPUT_NAME}/"
+rm -rf "${OUTPUT_NAME}/exercise-solutions/target"
+rm -rf "${OUTPUT_NAME}/exercise-solutions/connected-mailbox/target"
+zip -r "${OUTPUT_NAME}.zip" "${OUTPUT_NAME}"
