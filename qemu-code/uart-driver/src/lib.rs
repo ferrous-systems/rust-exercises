bc7dbe0b7cb48c6d293ae9f11ec4354e778f23a7
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2024-05-21 17:28:50 +0100

    I can just put the assembly inline for the QEMU example.

--- qemu-code/uart-driver/src/lib.rs
+++ qemu-code/uart-driver/src/lib.rs
@@ -38,3 +38,27 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
         }
     }
 }
+
+// This is our start-up code.
+core::arch::global_asm!(r"
+.section .text.startup
+.global _start
+.code 32
+.align 0
+
+_start:
+    // Set stack pointer
+    ldr r3, =stack_top
+    mov sp, r3
+    // Allow VFP coprocessor access
+    mrc p15, 0, r0, c1, c0, 2
+    orr r0, r0, #0xF00000
+    mcr p15, 0, r0, c1, c0, 2
+    // Enable VFP
+    mov r0, #0x40000000
+    vmsr fpexc, r0
+    // Jump to application
+    bl kmain
+    // In case the application returns, loop forever
+    b .
+");
