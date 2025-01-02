235d7e0c2718a6fcd30cbed2ed3018e7a08899e7
mrg <miguelraz@ciencias.unam.mx>
2025-01-02 13:29:01 -0600

    working autocxx local demo

--- exercise-templates/cpp-interop/src/main.rs
+++ exercise-templates/cpp-interop/src/main.rs
@@ -1,3 +1,26 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use autocxx::prelude::*;
+include_cpp! {
+    #include "input.h"
+    safety!(unsafe_ffi)
+    generate!("DoMath")
+    generate!("Goat")
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! - C++ math should say 12={}", ffi::DoMath(4));
+    let mut goat = ffi::Goat::new().within_box();
+    goat.as_mut().add_a_horn();
+    goat.as_mut().add_a_horn();
+    assert_eq!(
+        goat.describe().as_ref().unwrap().to_string_lossy(),
+        "This goat has 2 horns."
+    );
 }
