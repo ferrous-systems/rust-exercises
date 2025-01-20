32961683a669629f89ea4df6b1b422d3d2ba8294
Miguel Raz Guzmán Macedo <miguelraz@ferrous-systems.com>
2025-01-20 07:43:34 -0600

    remove unwrap() in solution
    Co-authored-by: Andrei Listochkin (Андрей Листочкин) <andrei.listochkin@ferrous-systems.com>

--- exercise-solutions/iterators/src/bin/iterators1.rs
+++ exercise-solutions/iterators/src/bin/iterators1.rs
@@ -24,7 +24,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Idiomatic solution
     let second_reader = BufReader::new(File::open("../exercise-solutions/iterators/numbers.txt")?);
     let nicer_sum: i32 = second_reader.lines()
-        .map(|l| l.unwrap())              
+        .filter_map(|line| line.ok())
         .filter_map(|s| s.parse().ok())   // map a .parse() and filter for the succesful parses
         .filter(|num| num % 2 != 0)
         .sum::<i32>();
