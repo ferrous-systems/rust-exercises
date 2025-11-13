f93e6ebcdbb4404c926edef741278ae740ef30fe
Robin Mueller <robin.mueller.m@pm.me>
2025-11-13 22:49:47 +0100

    add copyright notice and mention code copied from the nrf52840 HAL

--- nrf52-code/boards/dk-solution/src/radio.rs
+++ nrf52-code/boards/dk-solution/src/radio.rs
@@ -1,4 +1,32 @@
-//! IEEE 802.15.4 radio driver
+//! # IEEE 802.15.4 radio driver
+//!
+//! This driver is a synchronous/blocking one written for a radio training.
+//! A lot of its implementation is based on the
+//! [nrf52840 HAL](https://github.com/nrf-rs/nrf-hal)
+//!
+//! [MIT license from the project](https://github.com/nrf-rs/nrf-hal/blob/master/LICENSE-MIT):
+//!
+//! Copyright (c) 2018 Anthony James Munns
+//!
+//! Permission is hereby granted, free of charge, to any
+//! person obtaining a copy of this software and associated
+//! documentation files (the "Software"), to deal in the
+//! Software without restriction, including without
+//! limitation the rights to use, copy, modify, merge,
+//! publish, distribute, sublicense, and/or sell copies of
+//! the Software, and to permit persons to whom the Software
+//! is furnished to do so, subject to the following
+//! conditions:
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+//! ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//! TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//! PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+//! SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//! CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//! OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//! IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//! DEALINGS IN THE SOFTWARE.
 
 use core::sync::atomic::{compiler_fence, Ordering};
 
