16fb91b6fc333efc693616ac14b9e7f816d26a15
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2026-05-21 18:01:33 +0100

    Rename a green-yellow test case for consistency.
    Now the name matches the argument order.

--- exercise-solutions/green-yellow/src/bin/step5.rs
+++ exercise-solutions/green-yellow/src/bin/step5.rs
@@ -139,7 +139,7 @@ fn four_in_guess_one_in_secret() {
 }
 
 #[test]
-fn two_in_secret_one_in_guess() {
+fn one_in_guess_two_in_secret() {
     assert_eq!(
         &calc_green_and_yellow(&[1, 2, 3, 4], &[3, 3, 9, 9]),
         "⬜⬜🟨⬜"
